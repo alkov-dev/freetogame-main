6b3b84029e767a2e6c1a3053d93877af9c1037bb
alkov-dev <darkzoner@list.ru>
2022-01-31 15:18:07 +0300

    add

--- src/hooks/useTypeSelector.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { RootState } from "../redux/reducers";
-
-
-
-export const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/redux/index.ts
+++
@@ -1,7 +0,0 @@
-import { rootReducer } from './reducers/index';
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
